931f5572641e61c72ffd225514d01263d838c3b2
Aaron Lagdameo <aaron.kristoffer.lagdameo@oocl.com>
2019-09-30 07:29:16 +0800

    adjusted imported jquery script

--- js/todo.js
+++ js/todo.js
@@ -1,25 +1,22 @@
-$(document)
-    .ready(function () {
+$(document).ready(function () {
 
-        function generateUUID() {
-            /*jshint bitwise:false */
-            var i,
-                random;
-            var uuid = '';
+    function generateUUID() {
+        /*jshint bitwise:false */
+        var i,
+            random;
+        var uuid = '';
 
-            for (i = 0; i < 32; i++) {
-                random = Math.random() * 16 | 0;
-                if (i === 8 || i === 12 || i === 16 || i === 20) {
-                    uuid += '-';
-                }
-                uuid += (i === 12
-                    ? 4
-                    : (i === 16
-                        ? (random & 3 | 8)
-                        : random)).toString(16);
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+            if (i === 8 || i === 12 || i === 16 || i === 20) {
+                uuid += '-';
             }
-            return uuid;
+            uuid += (i === 12 ?
+                4 :
+                (i === 16 ?
+                    (random & 3 | 8) :
+                    random)).toString(16);
         }
-
-        // code to be implemented
-    });
+        return uuid;
+    }
+});
