6a946ff8b4273b478e83741f1621ced94e4f4591
Aaron Lagdameo <aaron.kristoffer.lagdameo@oocl.com>
2019-09-30 09:56:49 +0800

    included add button functionality

--- js/todo.js
+++ js/todo.js
@@ -19,4 +19,17 @@ $(document).ready(function () {
         }
         return uuid;
     }
+
+    let addButton = $('div[id=button]');
+    let taskList = $('ol[id=tasks]');
+
+    addButton.on('click', function () {
+        let task = $('input[name=ListItem]');
+        let taskItem = '<li id="' + generateUUID() + '" class="unchecked">' +
+            '<input name="done-todo" type="checkbox" class="done-todo">' +
+            task.val() + '</li>';
+
+        taskList.append(taskItem);
+        $(task).val('');
+    });
 });
